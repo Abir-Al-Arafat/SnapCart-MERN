96df08bd514862635752dec635b57b71d4143822
Abir-Al-Arafat <antorabir@gmail.com>
2024-01-05 23:26:18 +0600

    routing added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import { Container } from "react-bootstrap";
+import {Outlet} from 'react-router-dom'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (<>
     <Header/>
     <main className="py-3">
       <Container>
-        <h1>Welcome to SnapCart</h1>
-        <HomeScreen/>
+        {/* <h1>Welcome to SnapCart</h1> */}
+        <Outlet/>
       </Container>
     </main>
     <Footer/>
