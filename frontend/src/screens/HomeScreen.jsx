34de0a4b0946a49c2dc7b0cd6ebba17011c1bde6
Abir-Al-Arafat <antorabir@gmail.com>
2024-01-19 02:00:56 +0600

    fetching product details using RTK

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,29 +1,39 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
+  // const [products, setProducts] = useState([]);
 
-  const fetchProducts = async () => {
-    const { data } = await axios.get("/api/products");
-    setProducts(data);
-  };
+  // const fetchProducts = async () => {
+  //   const { data } = await axios.get("/api/products");
+  //   setProducts(data);
+  // };
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+  // useEffect(() => {
+  //   fetchProducts();
+  // }, []);
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h1>{error?.data?.message || error.error}</h1>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -3,20 +3,27 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import Rating from "../components/Rating";
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
 
 const ProductScreen = () => {
-  const [product, setProduct] = useState([]);
+  // const [product, setProduct] = useState([]);
   const { id: productId } = useParams();
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
+
   // console.log(products);
-  console.log("productId", productId);
-  const fetchProduct = async () => {
-    const { data } = await axios.get(`/api/products/${productId}`);
-    console.log("data", data);
-    setProduct(data);
-  };
-  useEffect(() => {
-    fetchProduct();
-  }, [productId]);
+  // console.log("productId", productId);
+  // const fetchProduct = async () => {
+  //   const { data } = await axios.get(`/api/products/${productId}`);
+  //   console.log("data", data);
+  //   setProduct(data);
+  // };
+  // useEffect(() => {
+  //   fetchProduct();
+  // }, [productId]);
   // const product = products.find((product) => product._id === productId);
   console.log("product", product);
   return (
@@ -25,73 +32,79 @@ const ProductScreen = () => {
         Go Back
       </Link>
       {/* <p>{JSON.stringify(product)}</p> */}
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={4}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              {" "}
-              <strong>Description: </strong> {product.description}
-            </ListGroup.Item>
-            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup>
+      {isLoading ? (
+        <h1>Loading....</h1>
+      ) : error ? (
+        <h1>{error?.data?.message || error.error}</h1>
+      ) : (
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
+            <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  {" "}
-                  <Col>Price:</Col>{" "}
-                  <Col>
-                    <strong>${product.price}</strong>{" "}
-                  </Col>{" "}
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  {" "}
-                  <Col>Status:</Col>{" "}
-                  <Col>
-                    <strong>
-                      {product.countInStock > 0 ? `in stock` : `out of stock`}
-                    </strong>{" "}
-                  </Col>{" "}
-                </Row>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  {" "}
-                  <Col>
-                    <div className="d-grid gap-2">
-                      <Button
-                        className="btn-block"
-                        type="button"
-                        disabled={product.countInStock === 0}
-                      >
-                        {product.countInStock > 0
-                          ? `Add to cart`
-                          : `not available`}
-                      </Button>
-                    </div>
-                  </Col>{" "}
-                </Row>
+                {" "}
+                <strong>Description: </strong> {product.description}
               </ListGroup.Item>
+              <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Row>
+                    {" "}
+                    <Col>Price:</Col>{" "}
+                    <Col>
+                      <strong>${product.price}</strong>{" "}
+                    </Col>{" "}
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    {" "}
+                    <Col>Status:</Col>{" "}
+                    <Col>
+                      <strong>
+                        {product.countInStock > 0 ? `in stock` : `out of stock`}
+                      </strong>{" "}
+                    </Col>{" "}
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    {" "}
+                    <Col>
+                      <div className="d-grid gap-2">
+                        <Button
+                          className="btn-block"
+                          type="button"
+                          disabled={product.countInStock === 0}
+                        >
+                          {product.countInStock > 0
+                            ? `Add to cart`
+                            : `not available`}
+                        </Button>
+                      </div>
+                    </Col>{" "}
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
