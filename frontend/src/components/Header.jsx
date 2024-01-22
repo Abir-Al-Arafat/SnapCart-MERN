2f93086989c40a57f3aef022304172717bf074ad
Abir-Al-Arafat <antorabir@gmail.com>
2024-01-22 18:54:32 +0600

    Number of products in cart added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 const Header = () => {
+  const cartItems = useSelector((state) => state.cart);
+
+  console.log("cartItems", cartItems.totalProducts);
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -20,6 +24,17 @@ const Header = () => {
                 <Nav.Link>
                   <FaShoppingCart />
                   Cart
+                  {cartItems?.totalProducts && (
+                    <Badge
+                      style={{
+                        marginLeft: "5px",
+                      }}
+                      bg="success"
+                      pill
+                    >
+                      {cartItems.totalProducts}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
               <LinkContainer to="/login">
